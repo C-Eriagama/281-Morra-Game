23db1c7b6dc3d3a6257b7d59f08390adc3118180
Chanul <ceri930@aucklanduni.ac.nz>
2023-05-06 18:47:40 +1200

    Allowed multiple inputs until input is valid for play

--- src/main/java/nz/ac/auckland/se281/Morra.java
+++ src/main/java/nz/ac/auckland/se281/Morra.java
@@ -4,42 +4,67 @@ import nz.ac.auckland.se281.Main.Difficulty;
 
 public class Morra {
 
+  // Game Variables
   private int round = 0;
+  private String name = "";
 
-  public Morra() {}
+  public Morra() {
+  }
 
   public void newGame(Difficulty difficulty, int pointsToWin, String[] options) {
-    MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+    name = options[0];
+    MessageCli.WELCOME_PLAYER.printMessage(name);
   }
 
   public void play() {
-    //Start new round and ask for input
+
+    // Start new round and ask for input
     round++;
     MessageCli.START_ROUND.printMessage(Integer.toString(round));
     MessageCli.ASK_INPUT.printMessage();
+    boolean error = true;
 
-    //Get input from user
-    String input = Utils.scanner.nextLine();
-    String fingerSum [] = input.split(" ");
-    String playerFingersString = fingerSum[0];
-    String playerSumString = fingerSum[1];
+    // Initialize variables
+    String input = "";
+    String fingerSum[] = new String[2];
+    String playerFingersString = "";
+    String playerSumString = "";
 
-    //error checking
-    if(!Utils.isInteger(playerFingersString) || !Utils.isInteger(playerSumString)){
-      MessageCli.INVALID_INPUT.printMessage();
-      return;
+    // error checking
+    while (error) {
+
+      // Get input from user
+      input = Utils.scanner.nextLine();
+      fingerSum = input.split(" ");
+      playerFingersString = fingerSum[0];
+      playerSumString = fingerSum[1];
+
+      // Check if input are integers
+      if (!Utils.isInteger(playerFingersString) || !Utils.isInteger(playerSumString)) {
+        MessageCli.INVALID_INPUT.printMessage();
+        error = true;
+        continue;
+      }
+
+      int playerFingers = Integer.parseInt(playerFingersString);
+      int playerSum = Integer.parseInt(playerSumString);
+
+      // Check if inputs are within range
+      if (playerFingers < 1 || playerFingers > 5 || playerSum < 1 || playerSum > 10) {
+        MessageCli.INVALID_INPUT.printMessage();
+        error = true;
+        continue;
+      }
+
+      // if no errors break out of loop
+      error = false;
     }
 
-    int playerFingers = Integer.parseInt(playerFingersString);
-    int playerSum = Integer.parseInt(playerSumString);
-
-    if(playerFingers < 1 || playerFingers > 5 || playerSum < 1 || playerSum > 10){
-      MessageCli.INVALID_INPUT.printMessage();
-      return;
-    }
-
+    // Print out input if passes error checking
+    MessageCli.PRINT_INFO_HAND.printMessage(name, playerFingersString, playerSumString);
 
   }
 
-  public void showStats() {}
+  public void showStats() {
+  }
 }
