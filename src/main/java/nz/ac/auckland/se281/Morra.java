edac3951fa350fd1f187267a33029c1a17d63cc3
Chanul <ceri930@aucklanduni.ac.nz>
2023-05-06 17:59:14 +1200

    Added Round start information and getting input from user

--- src/main/java/nz/ac/auckland/se281/Morra.java
+++ src/main/java/nz/ac/auckland/se281/Morra.java
@@ -4,13 +4,33 @@ import nz.ac.auckland.se281.Main.Difficulty;
 
 public class Morra {
 
+  private int round = 0;
+
   public Morra() {}
 
   public void newGame(Difficulty difficulty, int pointsToWin, String[] options) {
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
   }
 
-  public void play() {}
+  public void play() {
+    //Start new round and ask for input
+    round++;
+    MessageCli.START_ROUND.printMessage(Integer.toString(round));
+    MessageCli.ASK_INPUT.printMessage();
+
+    //Get input from user
+    String input = Utils.scanner.nextLine();
+    String fingerSum [] = input.split(" ");
+    String playerFingersString = fingerSum[0];
+    String playerSumString = fingerSum[1];
+
+    //error checking
+    if(!Utils.isInteger(playerFingersString) || !Utils.isInteger(playerSumString)){
+      MessageCli.INVALID_INPUT.printMessage();
+      return;
+    }
+
+  }
 
   public void showStats() {}
 }
